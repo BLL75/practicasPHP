8c58488ac683ff96c8b7e949c6bcf1a7499f275e
Bernardo <bluquelafuente@gmail.com>
2015-01-15 20:51:31 +0100

    commit servidorJSONtobeContinued

---
+++ servidorJSON/index.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script>
+	  // Obtener la instancia del objeto XMLHttpRequest
+	$(document).ready(function(){
+	    
+		
+		
+		$.ajax({
+			url: "cargaProvinciasJSON.php",
+			type: 'GET',
+			dataType: 'json',
+			success: function(data){
+			$(data).each(function(){
+		     //$("#selectProvincia").append("<option>" + $(this).find('nombre').text() + "</option>")
+			 $('#selectProvincia').append("<option value = '" + $(this).find('codigo').text() +"'>" + $(this).find('nombre').text() + "</option>");
+			 
+		   });}
+		});
+
+		
+		
+		$('#selectProvincia').change(function(){
+		    valor = $('#selectProvincia').val();
+			$('#selectLocalidad').empty();
+		    $.ajax({
+			  url: 'cargaMunicipiosXML.php',
+			  type: 'POST',
+			  async: true,
+			  data: 'provincia=' + valor,
+			  success: function(data){$(data).find('municipio').each(function(){
+				          $('#selectLocalidad').append("<option>" + $(this).find('nombre').text() + "</option>");
+			           });}
+			});
+        });		
+	});
+	
+	</script>
+	</head>
+	<body>
+		<?php
+			echo "<form method='POST' action='index.php' id='formulario'>";
+			//Comienzo select Provincia
+				echo "Provincia"."<select id = 'selectProvincia'>";
+				    echo "<option>-Provincia-</option>";
+				//Fin select Provincia
+				echo "</select>";
+			echo "</form>";	
+				
+			//Comienzo select Localidad	
+			echo "Localidad "."<select id = 'selectLocalidad'>";
+				echo "<option>-Localidad-</option>";
+				//Fin select Localidad
+			echo "</select>";
+            		
+		?>
+		
+	</body>
+</html>
